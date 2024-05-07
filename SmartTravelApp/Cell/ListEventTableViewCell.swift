83925f1c474ce22a5e1f6f0d19c86ee2796bbaa8
mac <yukchunma5@gmail.com>
2024-05-07 23:08:23 +0800

    test02

---
+++ SmartTravelApp/Cell/ListEventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListEventTableViewCell.swift
+//  SmartTravelApp
+//
+//  Created by student on 7/5/2024.
+//
+
+import UIKit
+
+class ListEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
