9293043cdb63c35efea9abab7b8f95899183f4f5
jiexiaozhang <yukchunma5@gmail.com>
2024-05-27 02:12:31 +0800

    New favorite management view controller

---
+++ SmartTravelApp/Cell/favoritemanageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  favoritemanageTableViewCell.swift
+//  SmartTravelApp
+//
+//  Created by student on 27/5/2024.
+//
+
+import UIKit
+
+class favoritemanageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SmartTravelApp/favoritemanageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  favoritemanageViewController.swift
+//  SmartTravelApp
+//
+//  Created by student on 27/5/2024.
+//
+
+import UIKit
+
+class favoritemanageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
